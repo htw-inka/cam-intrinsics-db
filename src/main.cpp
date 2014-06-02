12baf7e7c088aa1a940b6b64de48e3219713fac1
Markus Konrad <post@mkonrad.net>
2014-06-02 18:17:51 +0200

    dir/file handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -12,11 +14,14 @@
 using namespace cv;
 using namespace std;
 
-#define DEVICE_STRLEN   255
+#define DEVICE_STRLEN        256
+#define DEVICE_PATH_STRLEN   512
 
 float square_size = 0.0f;
 char device[DEVICE_STRLEN];
 bool all_devices = true;
+
+bool status_ok = true;
 
 vector<vector<Point2f> > imagePoints;
 Mat cameraMatrix, distCoeffs;
@@ -31,6 +36,65 @@ void printHelp() {
     cout << "- device" << endl;
 }
 
+void err(const char *msg) {
+    cerr << "ERROR: " << msg << endl;
+    status_ok = false;
+}
+
+void calibrate_device(const char *device) {
+    cout << "calibrating device '" << device << "'..." << endl;
+    
+    char path[DEVICE_PATH_STRLEN];
+    sprintf(path, "./device_photos/%s", device);
+    
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        err("could not open the device's photos directory");
+        return;
+    }
+    
+    struct dirent *dir_item;
+    
+    while ((dir_item = readdir(dir)) != NULL) {
+        if (!dir_item->d_name
+         || dir_item->d_name[0] == '.'
+         || dir_item->d_type != DT_REG)
+            continue;
+        
+        char file[DEVICE_PATH_STRLEN];
+        sprintf(file, "%s/%s", path, dir_item->d_name);
+        
+        cout << "> working with file '" << file << "'" << endl;
+    }
+}
+
+void calibrate_all() {
+    DIR *dir = opendir("./device_photos");
+    if (dir == NULL) {
+        err("could not open photos directory");
+        return;
+    }
+    
+    struct dirent *dir_item;
+    
+    while ((dir_item = readdir(dir)) != NULL) {
+        if (!dir_item->d_name
+         || dir_item->d_name[0] == '.'
+         || dir_item->d_type != DT_DIR)
+            continue;
+    
+        calibrate_device(dir_item->d_name);
+//        cout << "file: " << dir_item->d_name << endl;
+//        cout << " is folder: " << (dir_item->d_type == DT_DIR) << endl;
+    }
+    
+    closedir(dir);
+}
+
+void write_output() {
+
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printHelp();
@@ -41,7 +105,7 @@ int main(int argc, char *argv[]) {
     square_size = (float)strtod(argv[1], NULL);
     
     if (square_size <= 0.0f) {
-        cout << "unable to parse first argument as square size" << endl;
+        err("unable to parse first argument as square size");
         printHelp();
         
         return 2;
@@ -54,10 +118,23 @@ int main(int argc, char *argv[]) {
     
     cout << "using square size of " << square_size << " meters" << endl;
     cout << "generating camera intrinsics for ";
-    if (all_devices)
+    if (all_devices) {
         cout << "all devices" << endl;
-    else
+        
+        calibrate_all();
+    } else {
         cout << "device '" << device << "'" << endl;
+        
+        calibrate_device(device);
+    }
+
+    if (status_ok) {
+        write_output();
+    } else {
+        cerr << "calibration failed" << endl;
+    
+        return 3;
+    }
 
     return 0;
 }
