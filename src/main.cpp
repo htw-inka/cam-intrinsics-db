783b1c5c1d8aa81e87d3cab69306ad2c5dcc8490
Markus Konrad <post@mkonrad.net>
2014-06-02 17:22:42 +0200

    argument handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -10,6 +11,12 @@
 
 using namespace cv;
 using namespace std;
+
+#define DEVICE_STRLEN   255
+
+static float square_size = 0.0f;
+static char device[DEVICE_STRLEN];
+static bool all_devices = true;
 
 static void printHelp() {
     cout << "required arguments:" << endl;
@@ -20,15 +27,32 @@ static void printHelp() {
 }
 
 int main(int argc, char *argv[]) {
-    cout << argc << endl;
-
-    if (argc < 1) {
+    if (argc < 2) {
         printHelp();
         
         return 1;
     }
     
+    square_size = (float)strtod(argv[1], NULL);
     
+    if (square_size <= 0.0f) {
+        cout << "unable to parse first argument as square size" << endl;
+        printHelp();
+        
+        return 2;
+    }
+    
+    if (argc >= 3) {
+        strncpy(device, argv[2], DEVICE_STRLEN);
+        all_devices = false;
+    }
+    
+    cout << "using square size of " << square_size << " meters" << endl;
+    cout << "generating camera intrinsics for ";
+    if (all_devices)
+        cout << "all devices" << endl;
+    else
+        cout << "device '" << device << "'" << endl;
 
     return 0;
 }
