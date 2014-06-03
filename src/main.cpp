59ce19ffbb4b34259adca11b8b532c5b1ae31e30
Markus Konrad <post@mkonrad.net>
2014-06-03 14:47:01 +0200

    added basic calibration

--- src/main.cpp
+++ src/main.cpp
@@ -44,9 +44,11 @@ Size board_size(9,6);   // number of *inner* corners per a chessboard row and co
 
 bool status_ok = true;
 
-vector<vector<Point2f> > imagePoints;
-Mat cameraMatrix, distCoeffs;
-Size imageSize;
+vector<vector<Point2f> > img_pts;
+vector<Point3f> std_obj_pts;
+
+Mat cam_mat, dist_mat;
+Size img_size(0, 0);
 
 
 /** FUNCTIONS **/
@@ -64,10 +66,10 @@ void err(const char *msg) {
     status_ok = false;
 }
 
-bool calibrate_with_img(Mat& img) {
-    vector<Point2f> pointBuf;
+bool find_corners_in_img(Mat& img) {
+    vector<Point2f> point_buf;
 
-    bool found = findChessboardCorners(img, board_size, pointBuf,
+    bool found = findChessboardCorners(img, board_size, point_buf,
                                        CV_CALIB_CB_ADAPTIVE_THRESH
                                        | CV_CALIB_CB_FAST_CHECK
                                        | CV_CALIB_CB_NORMALIZE_IMAGE);
@@ -76,20 +78,55 @@ bool calibrate_with_img(Mat& img) {
         return false;
     }
     
-    Mat imgGray;
-    cvtColor(img, imgGray, COLOR_BGR2GRAY);
-    cornerSubPix(imgGray, pointBuf, Size(11,11),
+    Mat img_gray;
+    cvtColor(img, img_gray, COLOR_BGR2GRAY);
+    cornerSubPix(img_gray, point_buf, Size(11,11),
                  Size(-1,-1), TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 30, 0.1));
 
     if (graphical_disp) {
-        drawChessboardCorners(img, board_size, Mat(pointBuf), found);
+        drawChessboardCorners(img, board_size, Mat(point_buf), found);
     }
+    
+    Size new_img_size = img.size();
+    
+    if (img_size.width > 0 && img_size.height > 0
+     && img_size.width != new_img_size.width
+     && img_size.height != new_img_size.height)
+    {
+        err("all images of one device must have the same dimension");
+        return false;
+    }
+    
+    img_size = new_img_size;
+    img_pts.push_back(point_buf);
     
     return true;
 }
 
+bool run_calibration_with_data(vector<vector<Point2f> > pts, double *reproj_err) {
+    assert(reproj_err);
+
+    // initialize
+    cam_mat = Mat::eye(3, 3, CV_64F);
+    dist_mat = Mat::zeros(8, 1, CV_64F);
+    vector<Mat> rvecs;
+    vector<Mat> tvecs;
+    
+    // fill obj_pts with std_obj_pts elements so that it has the same size as img_pts
+    vector<vector<Point3f> > obj_pts;
+    obj_pts.resize(img_pts.size(), std_obj_pts);
+    
+    //Find intrinsic and extrinsic camera parameters
+    *reproj_err = calibrateCamera(obj_pts, img_pts, img_size, cam_mat,
+                                  dist_mat, rvecs, tvecs, CV_CALIB_FIX_K4|CV_CALIB_FIX_K5);
+    
+    return checkRange(cam_mat) && checkRange(dist_mat);
+}
+
 void calibrate_device(const char *device) {
     cout << "calibrating device '" << device << "'..." << endl;
+    
+    img_size = Size(0, 0);
     
     char path[DEVICE_PATH_STRLEN];
     sprintf(path, "./device_data/%s", device);
@@ -102,7 +139,10 @@ void calibrate_device(const char *device) {
     
     struct dirent *dir_item;
     
-    while ((dir_item = readdir(dir)) != NULL) {
+    int num_img = 0;
+    int num_img_ok = 0;
+    
+    while (status_ok && (dir_item = readdir(dir)) != NULL) {
         if (!dir_item->d_name
          || dir_item->d_name[0] == '.'
          || dir_item->d_type != DT_REG)
@@ -113,22 +153,48 @@ void calibrate_device(const char *device) {
         
         cout << "> working with file '" << file << "'" << endl;
         
+        num_img++;
+        
         Mat img = imread(file, CV_LOAD_IMAGE_COLOR);
         
         if (!img.data || img.rows == 0 || img.cols == 0) {
             err("image could not be loaded");
-            return;
+            break;
         }
         
-        bool res = calibrate_with_img(img);
+        bool res = find_corners_in_img(img);
         
-        cout << ">> calibration: " << (res ? "ok" : "failed") << endl;
+        cout << ">> finding chessboard corners: " << (res ? "ok" : "failed") << endl;
         
-        if (res && graphical_disp) {
-            imshow("image view", img);
-            if (interactive) waitKey(0);
+        if (res) {
+            num_img_ok++;
+        
+            if (graphical_disp) {
+                imshow("image view", img);
+                if (interactive) waitKey(0);
+            }
         }
     }
+        
+    cout << "using " << num_img_ok << " out of " << num_img << " for calibration" << endl;
+    
+    if (num_img_ok <= 0 || img_pts.size() <= 0) {
+        err("no chessboard corners found for calibration");
+        return;
+    }
+    
+    
+    cout << "calibrating device '" << device << "' using data set of " << img_pts.size() << endl;
+    
+    double reproj_err;
+    bool calib_ok = run_calibration_with_data(img_pts, &reproj_err);
+    
+    if (!calib_ok) {
+        cout << "calibration failed";
+        return;
+    }
+    
+    cout << "calibration succeeded with reprojection error " << reproj_err << endl;
 }
 
 void calibrate_all() {
@@ -147,6 +213,7 @@ void calibrate_all() {
             continue;
     
         calibrate_device(dir_item->d_name);
+        
 //        cout << "file: " << dir_item->d_name << endl;
 //        cout << " is folder: " << (dir_item->d_type == DT_DIR) << endl;
     }
@@ -156,6 +223,16 @@ void calibrate_all() {
 
 void write_output() {
 
+}
+
+void init() {
+    std_obj_pts.clear();
+    
+    for (int i = 0; i < board_size.height; ++i) {
+        for (int j = 0; j < board_size.width; ++j) {
+            std_obj_pts.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -197,8 +274,11 @@ int main(int argc, char *argv[]) {
         all_devices = false;
     }
     
+    init();
+    
     cout << "using square size of " << square_size << " meters" << endl;
     cout << "generating camera intrinsics for ";
+    
     if (all_devices) {
         cout << "all devices" << endl;
         
